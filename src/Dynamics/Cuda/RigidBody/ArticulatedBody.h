2f1b9a298f87ad4c0d47f108d42224917934c201
5jwind <wjwade959@163.com>
2025-11-22 15:29:35 +0800

    delete useless urdfParser in ArticulatedBody.h

--- src/Dynamics/Cuda/RigidBody/ArticulatedBody.h
+++ src/Dynamics/Cuda/RigidBody/ArticulatedBody.h
@@ -42,8 +42,6 @@ namespace dyno
 		//Bind rigid body to a shape in TextureMesh
 		void bindShape(std::shared_ptr<PdActor> actor, Pair<uint, uint> shapeId);
 
-		UrdfParser urdfParser;
-
 		UrdfInformation urdfInfo;
 
 	public:

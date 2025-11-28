dcc43ef06cf307fcccba51f627e7ba18e9c6a83f
5jwind <wjwade959@163.com>
2025-11-28 21:41:38 +0800

    clean the code

--- examples/Cuda/RigidBody/RobotArm_Impulse/RobotArm.cpp
+++ examples/Cuda/RigidBody/RobotArm_Impulse/RobotArm.cpp
@@ -129,12 +129,6 @@ namespace dyno
     void RobotArmSimulator<TDataType>::initialize(int width, int height, Real scale) {
         app.initialize(width, height);
         app.renderWindow()->getCamera()->setUnitScale(scale);
-        // std::cout << "Initializing..." << std::endl;
-        // isInitialized = true;
-        // activeScene = SceneGraphFactory::instance()->active();
-        // std::cout << "resetting scene..." << std::endl;
-        // activeScene->reset();
-        // std::cout << "Initialization Done!" << std::endl;
     }
 
     template<typename TDataType>
